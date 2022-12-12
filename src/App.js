dfa7bd32e4e2a06617eead0eb966474ae3cd41a0
unknown <gulnihalcengiz6@gmail.com>
2022-12-12 23:48:18 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ import Team from './components/pages/Team/Team';
 import Jobs from './components/pages/Jobs/Jobs';
 import Contact from './components/pages/Contact/Contact';
 // import Wedo from './components/pages/WhatWeDo/Wedo';
-import Family from './components/pages/Family/Family';
-import Dİvorce from './components/pages/Divorce/Dİvorce';
-import Accident from './components/pages/Accident/Accident';
-import Criminal from './components/pages/Criminal/Criminal';
-import Injury from './components/pages/Injury/Injury';
-import Wrongful from './components/pages/Wrongful/Wrongful';
+import Family from './components/pages/Jobs/Family';
+import Dİvorce from './components/pages/Jobs/Dİvorce';
+import Accident from './components/pages/Jobs/Accident';
+import Criminal from './components/pages/Jobs/Criminal';
+import Injury from './components/pages/Jobs/Injury';
+import Wrongful from './components/pages/Jobs/Wrongful';
 import Testimonial from './components/pages/Testimonial/Testimonial';
 
 function App() {
@@ -26,19 +26,20 @@ function App() {
     <>
       <Router>
         <Navbar />
+        <Home />
         <Routes>
-          <Route exact path='/Home' element={<Home /> }></Route>
+          {/* <Route exact path='/Home' element={<Home /> }></Route> */}
           <Route path='/Jobs' element={<Jobs /> } />
           <Route path='/Team' element={<Team /> } />     
           <Route path='/Contact' element={<Contact />} />
           {/* <Route path='/Wedo' element={<Wedo />} ></Route> */}
           <Route path='/Testimonial' element={<Testimonial />} />
-          <Route path='/Family' element={<Family />} ></Route>
-          <Route path='/Divorce' element={<Dİvorce />} ></Route>
-          <Route path='/Accident' element={<Accident />} ></Route>
-          <Route path='/Criminal' element={<Criminal />} ></Route>
-          <Route path='/Injury' element={<Injury />}></Route>
-          <Route path='/Wrongful' element={<Wrongful />}></Route>
+          <Route path='/Jobs/Family' element={<Family />} ></Route>
+          <Route path='/Jobs/Divorce' element={<Dİvorce />} ></Route>
+          <Route path='/Jobs/Accident' element={<Accident />} ></Route>
+          <Route path='/Jobs/Criminal' element={<Criminal />} ></Route>
+          <Route path='/Jobs/Injury' element={<Injury />}></Route>
+          <Route path='/Jobs/Wrongful' element={<Wrongful />}></Route>
         </Routes>
         <Footer />
         
